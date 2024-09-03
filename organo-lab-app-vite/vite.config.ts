36e52bf16e0c5958f066cf4627ceae6484228031
Aron-PC <aron.b.96@gmail.com>
2024-09-03 13:38:50 -0300

    move prettierrc and eslint config

--- organo-lab-app-vite/vite.config.ts
+++ organo-lab-app-vite/vite.config.ts
@@ -2,7 +2,4 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: false,
-  },
 });
